7965b1415d8be0b8e3287ff5787892949bab1de6
Grey Lovelace <austin.grey.lovelace@gmail.com>
2023-09-20 10:52:00 -0500

    Add template

--- src/llm.py
+++ src/llm.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain import OpenAI, LLMChain, PromptTemplate, SagemakerEndpoint
-from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains import RetrievalQAWithSourcesChain, RetrievalQA
 from src.vector_store import store
 from src.content_handler import ContentHandler
 
@@ -8,11 +8,28 @@ load_dotenv()
 
 # llm = OpenAI(temperature=0.1)
 llm = SagemakerEndpoint(
-    endpoint_name="meta-textgeneration-llama-2-7b-f-2023-09-19-21-55-05-593",
-    credentials_profile_name="AdministratorAccess-144406111952",
+    endpoint_name="meta-textgeneration-llama-2-7b-f-2023-09-20-13-35-02-360",
+    credentials_profile_name="sandbox-dev",
     region_name="us-east-1",
-    model_kwargs={"max_new_tokens": 700, "top_p": 0.9, "temperature": 0.1},
+    model_kwargs={"max_new_tokens": 700, "top_p": 0.9, "temperature": 0.2},
     endpoint_kwargs={"CustomAttributes": 'accept_eula=true'},
     content_handler=ContentHandler()
 )
-llm_chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=store.as_retriever())
+template = """
+    You are a world class researcher, excellent at answering quesions based on snippets of data from websites and pdfs.
+    Given the following snippets of data and a question, create a final answer.
+    If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+    
+    QUESTION: {question}
+    =========
+    CONTENT: {summaries}
+    =========
+    FINAL ANSWER:
+"""
+prompt = PromptTemplate(template=template, input_variables=['summaries', 'question'])
+llm_chain = RetrievalQAWithSourcesChain.from_llm(
+    llm=llm,
+    retriever=store.as_retriever(search_kwargs={"k": 3}),
+    combine_prompt=prompt,
+    return_source_documents=True,
+)
--- tests/1-ask.py
+++ tests/1-ask.py
@@ -5,5 +5,6 @@ from datetime import datetime
 print("Current sources")
 print(get_vector_sources())
 print(datetime.now())
-print(llm_chain({"question": "Who were Source Allies's black belts this year?"}))
+resp = llm_chain({"question": "Who were the Source Allies blackbelts in 2023?"})
+print(resp['answer'])
 print(datetime.now())
